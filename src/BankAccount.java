50a9ad858e77d3ee3eed927e0fc0679f85dc7f55
jgonsalves21 <42977061+jgonsalves21@users.noreply.github.com>
2018-09-26 13:37:54 -0400

    Add files via upload

---
+++ src/BankAccount.java
@@ -0,0 +1,26 @@
+
+public class BankAccount {
+
+	private String name;
+	
+	private int accNum;
+	
+	private double balance;
+	
+	public void holder() 
+	{
+		name = "Joshua Gonsalves";
+	}
+	
+	public int num(int a)
+	{
+		return accNum = a;
+	}
+	
+	pub
+	
+	
+	
+	
+	
+}
